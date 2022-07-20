35465500f3678f1089a523b515c00911bed5b32f
SUPRIT CHAUDHARI <supritchaudhari.iitg@gmail.com>
2022-07-20 11:11:45 +0530

    implement interfaces. closes #5

---
+++ Pet-Clinic/Pet-clinic-data/src/main/java/com/springboot/petclinic/services/PetService.java
@@ -0,0 +1,16 @@
+package com.springboot.petclinic.services;
+
+
+
+import com.springboot.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ Pet-Clinic/Pet-clinic-data/src/main/java/com/springboot/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.springboot.petclinic.services;
+
+import com.springboot.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
