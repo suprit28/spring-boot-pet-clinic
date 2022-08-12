69b38dad11820bd3aa0d4a6eb9ba27a5f756f4b4
SUPRIT CHAUDHARI <supritchaudhari.iitg@gmail.com>
2022-08-12 18:32:14 +0530

    updated vet to use JPA. closes #37

--- Pet-Clinic/Pet-clinic-data/src/main/java/com/springboot/petclinic/model/Speciality.java
+++ Pet-Clinic/Pet-clinic-data/src/main/java/com/springboot/petclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package com.springboot.petclinic.model;
 
-public class Speciality extends BaseEntity{
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "specialties")
+public class Speciality extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
